93a2efccf45cca9ea53d1fdc3fac9f8d28f3bf52
matheuefranco <matheusefranco@gmail.com>
2025-10-06 09:10:51 -0300

    feat:equals

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -23,6 +23,29 @@ public class Pessoa implements Comparable<Pessoa> {
     public String toString(){
         return "|" +this.matricula+ "-" + this.nome + "|";
     }
+    @Override
+    public int hashCode() {
+        return  matricula.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Pessoa other = (Pessoa) obj;
+        if (matricula == null) {
+            if (other.matricula != null)
+                return false;
+        } else if (!matricula.equals(other.matricula))
+            return false;
+        return true;
+    }
+
+    
     
 
 
